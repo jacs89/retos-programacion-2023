8ecfc6086d2c727b4b63375e213a694aeb0d380f
jacs89 <116157054+jacs89@users.noreply.github.com>
2024-07-07 17:50:45 +0200

    0.2

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/oldsire.js
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/oldsire.js
@@ -1,14 +1,20 @@
-for (let indice = 1; indice <= 100; indice++) {
 
-    if (indice % 3 === 0 && indice % 5 === 0){
-        console.log("FizBuzz")
-    }
-
-    if (indice % 3 === 0){
-        console.log("Fizz")
-    }
-
-    if (indice % 5 === 0) {
-        console.log("Buzz")
+function fizzbuzz() {
+    for (let indice = 1; indice <= 100; indice++) {
+    
+        if (indice % 3 === 0 && indice % 5 === 0){
+            console.log("FizBuzz")
+        }    
+        else if (indice % 3 === 0){
+            console.log("Fizz")
+        }    
+        else if (indice % 5 === 0) {
+            console.log("Buzz")
+        }    
+        else {
+            console.log("indice")
+        }
     }
 }
+
+fizzbuzz()
