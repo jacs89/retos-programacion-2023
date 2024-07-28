d67bc0e51eaeee1cff824742718f80fcd7041173
jacs89 <116157054+jacs89@users.noreply.github.com>
2024-07-28 14:10:49 +0200

    0.2

--- Retos/Reto #47 - LA PALABRA DE 100 PUNTOS [Fácil]/javascript/oldsire.js
+++ Retos/Reto #47 - LA PALABRA DE 100 PUNTOS [Fácil]/javascript/oldsire.js
@@ -1,54 +1,42 @@
-
 const readline = require('readline');
 
-//Donde guardo el abecedario
+// Donde guardo el abecedario
 const abecedario = "abcdefghijklmnñopqrstuvwxyz";
 
-
-//La función principal del juego
-function principal () {
-
+// La función principal del juego
+function principal() {
     const teclado = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
     teclado.question("Introduce una palabra de 100 puntos: ", (palabra) => {
-        
         let puntos = puntuarPalabra(palabra);
 
-        if (puntos === 100) { 
-            console.log('Ganaste.'); 
-        } else { 
-            console.log('Inténtelo de nuevo.'); 
+        if (puntos > 100) {
+            console.log('Ganaste. La palabra suma ' + puntos + ' puntos.');
+        } else {
+            console.log('Inténtelo de nuevo, esa palabra suma ' + puntos + ' puntos , no 100pts.');
         }
 
         teclado.close();
-
     });
-
 }
 
-
-//La función que emplearemos para puntuar la palabra
+// La función que emplearemos para puntuar la palabra
 function puntuarPalabra(palabra) {
-    
     let puntos = 0;
     palabra = palabra.toLowerCase();
 
     for (let i = 0; i < palabra.length; i++) {
-
         let indice = abecedario.indexOf(palabra[i]);
-        
         if (indice !== -1) {
-            puntos += indice + 1; //Los indices empiezan en 0
+            puntos += indice + 1; // Los índices empiezan en 0
         }
     }
 
     return puntos;
 }
 
-
-
-//Ejecución del juego
+// Ejecución del juego
 principal();
