e3e3542f3c19ef90ae51ab7863113e38c2c13ed8
jacs89 <116157054+jacs89@users.noreply.github.com>
2024-07-28 13:37:35 +0200

    Primer intento

---
+++ Retos/Reto #47 - LA PALABRA DE 100 PUNTOS [Fácil]/javascript/oldsire.js
@@ -0,0 +1,54 @@
+
+const readline = require('readline');
+
+//Donde guardo el abecedario
+const abecedario = "abcdefghijklmnñopqrstuvwxyz";
+
+
+//La función principal del juego
+function principal () {
+
+    const teclado = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    teclado.question("Introduce una palabra de 100 puntos: ", (palabra) => {
+        
+        let puntos = puntuarPalabra(palabra);
+
+        if (puntos === 100) { 
+            console.log('Ganaste.'); 
+        } else { 
+            console.log('Inténtelo de nuevo.'); 
+        }
+
+        teclado.close();
+
+    });
+
+}
+
+
+//La función que emplearemos para puntuar la palabra
+function puntuarPalabra(palabra) {
+    
+    let puntos = 0;
+    palabra = palabra.toLowerCase();
+
+    for (let i = 0; i < palabra.length; i++) {
+
+        let indice = abecedario.indexOf(palabra[i]);
+        
+        if (indice !== -1) {
+            puntos += indice + 1; //Los indices empiezan en 0
+        }
+    }
+
+    return puntos;
+}
+
+
+
+//Ejecución del juego
+principal();
